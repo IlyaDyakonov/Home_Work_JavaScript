8a77bfa17f7547fecb08bed4a8f2cbae3227bb35
Илья АриоХ <123810575+IlyaDyakonov@users.noreply.github.com>
2023-12-21 00:56:27 +0400

    Update task.js

--- 1_HW__JavaScript_the_basics_of_working_in_the_browser/2.2_Changing_HTML_document_structure/1. Визуальные подсказки/task.js
+++ 1_HW__JavaScript_the_basics_of_working_in_the_browser/2.2_Changing_HTML_document_structure/1. Визуальные подсказки/task.js
@@ -1,21 +1,37 @@
-const has_tooltip = document.querySelectorAll(".has-tooltip");
+const hasTooltip = document.querySelectorAll(".has-tooltip");
 
-has_tooltip.forEach((item) => {
+hasTooltip.forEach((item) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
 
+        // Получает значение 'title'
         const title = item.getAttribute('title');
+        const tooltip = item.parentNode.querySelector('.tooltip');
 
+        // проверка на открытие подсказки
+        if (tooltip && tooltip.classList.contains('tooltip_active')) {
+            tooltip.classList.remove('tooltip_active');
+        } else {
+        // Удаляет все элементы с классом "tooltip", что бы одновремено работала ток 1 подсказка
         document.querySelectorAll('.tooltip').forEach((tooltip) => {
             tooltip.remove();
         });
 
+        // Создание элемента и присовение класса и текста
         const div = document.createElement('div');
         div.className = 'tooltip';
         div.textContent = title;
+        document.body.appendChild(div);
 
+        const rect = item.getBoundingClientRect();
+        div.style.left = `${rect.left + window.scrollX}px`;
+        div.style.top = `${rect.top + window.scrollY + item.offsetHeight}px`;
+
+        // Вставляет созданный элемент div перед следующим элементом после текущего элемента item в его родительском элементе.
         item.parentNode.insertBefore(div, item.nextSibling);
 
+        // Добавляет или удаляет класс "tooltip_active" у созданного элемента div
         div.classList.toggle('tooltip_active');
+        }
     });
 });
