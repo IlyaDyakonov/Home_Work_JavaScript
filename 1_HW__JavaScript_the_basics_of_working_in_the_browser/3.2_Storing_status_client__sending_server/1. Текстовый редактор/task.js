f11db6f6a53a9c62c1befd38b15bae8b77835bb9
Илья АриоХ <123810575+IlyaDyakonov@users.noreply.github.com>
2024-01-01 14:36:20 +0400

    Update task.js 3.2 1

--- 1_HW__JavaScript_the_basics_of_working_in_the_browser/3.2_Storing_status_client__sending_server/1. Текстовый редактор/task.js
+++ 1_HW__JavaScript_the_basics_of_working_in_the_browser/3.2_Storing_status_client__sending_server/1. Текстовый редактор/task.js
@@ -2,18 +2,22 @@ const editor = document.getElementById('editor');
 const text = document.querySelector('textarea');
 const clearButton = document.querySelector('.clear')
 
+// Получаем значение из локального хранилища
 const storage = localStorage.getItem('textarea');
 
+// Если значение существует, устанавливаем его в поле ввода
 if (storage) {
     text.value = storage;
-} else {
+}
+// Добавляем обработчик события вне зависимости от значения в локальном хранилище
 text.addEventListener('input', () => {
+// Обновляем значение в локальном хранилище при изменении текста
     localStorage.setItem('textarea', text.value);
 });
-}
 
+// добавляем обработчик для кнопки очистки
 clearButton.addEventListener('click', () => {
+    // Удаляем значение из локального хранилища и очищаем поле ввода
     localStorage.removeItem('textarea');
     text.value = '';
 })
-
